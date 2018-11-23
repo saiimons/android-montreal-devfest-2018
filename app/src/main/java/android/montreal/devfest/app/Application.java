21edb56bbe17c91c8003ce5d2b406ba273da1939
Simon Guérout <saiimons@gmail.com>
2018-11-23 11:09:51 -0500

    Added test cases

--- app/src/main/java/android/montreal/devfest/app/Application.java
+++ app/src/main/java/android/montreal/devfest/app/Application.java
@@ -2,13 +2,10 @@ package android.montreal.devfest.app;
 
 import android.montreal.devfest.stuff.policies.DaggerSuffPoliciesComponent;
 
-import javax.inject.Named;
-
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
 
 public class Application extends DaggerApplication {
-    @Named("dependency")
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
         return DaggerApplicationComponent.builder()
